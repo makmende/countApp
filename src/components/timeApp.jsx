32e6cfc9df421f4a688b507f29abe539fb775556
Padre <144624630+padripio@users.noreply.github.com>
2025-06-04 17:50:53 +0000

    new-commit

---
+++ src/components/timeApp.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../styles.css";
+
+let time = new Date().toLocaleTimeString();
+
+function TimeApp() {
+  // create a time usestate
+  let [timeNow, setTime] = useState(time);
+
+  function updateTime() {
+    let newtime = new Date().toLocaleTimeString();
+    setTime(newtime);
+  }
+  setInterval(updateTime, 1000);
+  return (
+    <div>
+      <h1>{timeNow}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
+}
+export default TimeApp;
